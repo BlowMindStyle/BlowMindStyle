776f79b3e7c21848e6c5f891408c58624b09592a
Aleksey Gotyanov <Aleksey.Gotyanov@gmail.com>
2019-08-18 15:24:08 +0300

    text styles

---
+++ Sources/BlowMindStyle/Text/SemanticString.TextStyle+Extensions.swift
@@ -0,0 +1,11 @@
+public extension SemanticString.TextStyle {
+    static let body          = Self.init(rawValue: "body")
+    static let callout       = Self.init(rawValue: "callout")
+    static let caption       = Self.init(rawValue: "caption")
+    static let footnote      = Self.init(rawValue: "footnote")
+    static let headline      = Self.init(rawValue: "headline")
+    static let largeTitle    = Self.init(rawValue: "largeTitle")
+    static let subheadline   = Self.init(rawValue: "subheadline")
+    static let title         = Self.init(rawValue: "title")
+    static let bold          = Self.init(rawValue: "bold")
+}
