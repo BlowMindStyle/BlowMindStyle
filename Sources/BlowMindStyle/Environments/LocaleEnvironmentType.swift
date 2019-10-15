11501f1e33348938306c7904e3f02997e5cce6e0
Aleksey Gotyanov <Aleksey.Gotyanov@gmail.com>
2019-10-15 14:07:12 +0300

    fix preferred language

--- Sources/BlowMindStyle/Environments/LocaleEnvironmentType.swift
+++ Sources/BlowMindStyle/Environments/LocaleEnvironmentType.swift
@@ -31,6 +31,8 @@ public protocol LocaleEnvironmentType {
 
 public extension LocaleInfoType {
     func localize(_ resource: StringResourceType) -> String {
+        let lprojName = self.lprojName ?? Bundle.main.preferredLocalizations.first
+
         guard let languageBundlePath = resource.bundle.path(forResource: lprojName, ofType: "lproj"),
             let languageBundle = Bundle(path: languageBundlePath) else {
                 return resource.bundle.localizedString(forKey: resource.key, value: nil, table: resource.tableName)
